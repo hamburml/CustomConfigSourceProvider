b1a2bc7f6e58651fbd77f9735cc75fec7840cdc0
Michael Hamburger <michael.hamburger@pitforest.de>
2021-02-04 21:33:23 +0100

    example

--- config-service/src/main/java/de/example/config/ConfigResult.java
+++ config-service/src/main/java/de/example/config/ConfigResult.java
@@ -9,18 +9,10 @@ import javax.inject.Inject;
 public class ConfigResult {
 
     @Inject
-    @ConfigProperty(name = "test.property")
-    String applicationPropertiesProperty;
+    @ConfigProperty(name = "property")
+    String property;
 
-    @Inject
-    @ConfigProperty(name = "more.property")
-    String providerProperty;
-
-    public String getApplicationPropertiesProperty() {
-        return applicationPropertiesProperty;
-    }
-
-    public String getProviderProperty() {
-        return providerProperty;
+    public String getProperty() {
+        return property;
     }
 }
--- customconfigsourceprovider/src/main/java/de/example/config/CustomConfigSourceProvider.java
+++ customconfigsourceprovider/src/main/java/de/example/config/CustomConfigSourceProvider.java
@@ -3,6 +3,7 @@ package de.example.config;
 import io.smallrye.config.PropertiesConfigSource;
 import org.eclipse.microprofile.config.spi.ConfigSource;
 import org.eclipse.microprofile.config.spi.ConfigSourceProvider;
+import org.jboss.logging.Logger;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -13,17 +14,29 @@ import io.smallrye.common.classloader.ClassPathUtils;
 
 public class CustomConfigSourceProvider implements ConfigSourceProvider {
 
+
+    private static final Logger logger = Logger.getLogger(CustomConfigSourceProvider.class);
+
     @Override
     public Iterable<ConfigSource> getConfigSources(ClassLoader classLoader) {
         List<ConfigSource> configs = new ArrayList<>();
-        
+        logger.info("--- CustomConfigSourceProvider ---");
+
         try {
             Properties configProperties = new Properties();
 
-            ClassPathUtils.consumeAsStreams("more.properties", fileStream -> {
+            String propertyPrefix = this.getPropertyPrefix();
+
+            logger.info("propertyPrefix: " + propertyPrefix);
+            if(propertyPrefix == null)
+                propertyPrefix = "";
+
+            String file = propertyPrefix + "stage.properties";
+            logger.info("use property file: " + file);
+            ClassPathUtils.consumeAsStreams(file, fileStream -> {
                 try {
                     configProperties.load(fileStream);
-                    configs.add(new PropertiesConfigSource(configProperties, "more.properties"));
+                    configs.add(new PropertiesConfigSource(configProperties, ""));
                 }
                 catch (IOException e) {
                     e.printStackTrace();
@@ -35,5 +48,10 @@ public class CustomConfigSourceProvider implements ConfigSourceProvider {
         }
         
         return configs;
+    }
+
+    private String getPropertyPrefix()
+    {
+        return System.getProperty("stage");
     }
 }
